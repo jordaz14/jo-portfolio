7d9eac8da680464fd7b570a43ebfaddbdee3a766
Julian Ordaz <1julianordaz@gmail.com>
2023-11-12 18:26:14 -0500

    initial layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Oranienbaum } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const oranienbaum = Oranienbaum({ weight: ["400"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Julian Ordaz',
-  description: 'Welcome to my portfolio',
-}
+  title: "Julian Ordaz",
+  description: "My portfolio",
+  icons: {
+    icon: "/mountain.svg",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${oranienbaum.className} overflow-hidden`}>{children}</body>
     </html>
-  )
+  );
 }
